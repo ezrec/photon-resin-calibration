14852698e0877c8202afea2dac4ce80548c01cb6
Jason S. McMullan <jason.mcmullan@gmail.com>
2020-06-08 21:30:03 -0400

    example release workflow

---
+++ convert.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+trap 'catch $? $LINENO' EXIT
+
+catch() {
+  if [ "$1" != "0" ]; then
+    echo "Error $1 occurred on $2"
+    rm -rf ${ARCHIVE}
+    exit "$1"
+  fi
+}
+
+ARCHIVE=${1}
+shift
+FORMAT=${1}
+shift
+
+rm -rf ${ARCHIVE}
+mkdir -p ${ARCHIVE}
+cp -a resin-xp-finder/. ${ARCHIVE}/.
+# Skip the 'validation_print' directory
+rm -rf ${ARCHIVE}/validation_print
+
+find ${ARCHIVE} -name "*.photon" | (while read i; do
+    nn=$(dirname $i)/$(basename $i .photon).${FORMAT}
+    echo ":uv3dp ${i}" "$@" "${nn}"
+    go run github.com/ezrec/uv3dp/cmd/uv3dp ${i} "$@" ${nn}
+    rm -f ${i}
+done)
+
+rm -f ${ARCHIVE}.zip
+cd ${ARCHIVE}
+zip -r ../${ARCHIVE}.zip .
+cd ..
+rm -rf ${ARCHIVE}
